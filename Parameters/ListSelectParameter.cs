f6f4c4570f98efca67790c1cece25deb87ff9cbc
AidenPearceCore <qazasdfg15@gmail.com>
2022-04-07 13:06:44 +0800

    Revert "fixes: 修正前端驗證問題 (issue #1)"
    This reverts commit a838fa7c54dae02375c920e15c27d88e883e3082.

--- Parameters/ListSelectParameter.cs
+++ Parameters/ListSelectParameter.cs
@@ -1,10 +1,7 @@
-﻿using DemoWeb.ValidationAttributes;
-
-namespace DemoWeb.Parameters
+﻿namespace DemoWeb.Parameters
 {
     public class ListSelectParameter
     {
-        [ListSelectId]
         public int id { get; set; }
 
     }
--- Services/ListService.cs
+++ Services/ListService.cs
@@ -95,7 +95,6 @@ namespace DemoWeb.Services
                 update.Numberofrooms = value.Numberofrooms;
                 update.Price = value.Price;
                 update.Type = value.Type;
-                update.Floor = value.Floor;
                 _demoDatabaseContext.SaveChanges();
             }
 
